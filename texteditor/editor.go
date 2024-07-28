9e92ca04324df329dde71af397204cd3d294f59b
sleepy-day <matthew@sleepy.day>
2024-07-28 13:53:47 +1000

    refactored editor into struct and removed tcell init

--- texteditor/editor.go
+++ texteditor/editor.go
@@ -1,212 +1,218 @@
 package texteditor
 
 import (
-	"fmt"
-
 	"github.com/gdamore/tcell/v2"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/charmap"
 )
 
-var (
-	screen     tcell.Screen
-	x, y       int               = 0, 0
-	prevX      int               = 0
-	maxX, maxY int               = 0, 0
-	buffer     []byte            = []byte("test\ntesttest\n      tester5\n\n\n testing once more\n")
-	decoder    *encoding.Decoder = charmap.ISO8859_1.NewDecoder()
-
-	lineStart, lineEnd int = 0, 0
-	lines              [][]rune
-	lineLengths        []int
-
+type Editor struct {
 	gap *GapBuffer
 
-	updateLines bool = false
+	xPos, yPos int
+	maxX, maxY int
+	curX, curY int
 
-	defStyle tcell.Style = tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
-)
+	firstLn, lastLn int
+	lines           [][]rune
+	lineLengths     []int
+	updateLines     bool
+	prevX           int
 
-func Start(buf []byte) {
-	var err error
-	screen, err = tcell.NewScreen()
-	if err != nil {
-		panic(err.Error())
+	style *tcell.Style
+
+	focused bool
+}
+
+func CreateEditor(x, y, maxX, maxY int, buf []byte, style *tcell.Style) *Editor {
+	var gap *GapBuffer
+	if buf != nil {
+		gap = CreateGapBuffer(buf, 4000)
+	} else {
+		gap = CreateGapBuffer([]byte{}, 4000)
 	}
 
-	err = screen.Init()
-	if err != nil {
-		panic(err.Error())
+	return &Editor{
+		xPos:  x,
+		yPos:  y,
+		maxX:  maxX,
+		maxY:  maxY,
+		style: style,
+		gap:   gap,
 	}
+}
 
-	gap = CreateGapBuffer(buf, 2000)
-	lines = gap.GetLines(1, gap.GetLineCount())
+func (edit *Editor) HandleInput(ev tcell.Event) {
+	switch event := ev.(type) {
+	case *tcell.EventKey:
+		switch {
+		case event.Key() == tcell.KeyUp:
+			if edit.curY != 0 {
+				edit.curY--
+				edit.gap.MoveUp()
 
-	for _, v := range lines {
-		lineLengths = append(lineLengths, len(v))
-	}
+				if edit.gap.prevX < 0 {
+					edit.gap.prevX = edit.curX
+				}
 
-	screen.SetStyle(defStyle)
-
-	for i, v := range lines {
-		var rest []rune = nil
-		if len(v) > 1 {
-			rest = v[1:]
-		}
-		screen.SetContent(0, i, v[0], rest, defStyle)
-	}
-
-	lineEnd = len(lines)
-
-	maxX, maxY = screen.Size()
-
-	screen.SetCursorStyle(tcell.CursorStyleBlinkingBlock)
-	screen.ShowCursor(x, y)
-
-	for {
-		screen.Show()
-
-		ev := screen.PollEvent()
-
-		switch event := ev.(type) {
-		case *tcell.EventResize:
-			screen.Sync()
-			_, maxY = screen.Size()
-		case *tcell.EventKey:
-			debugInfo()
-			switch {
-			case event.Key() == tcell.KeyCtrlC:
-				screen.Fini()
-				return
-			case event.Key() == tcell.KeyUp:
-				if y != 0 {
-					y--
-					gap.MoveUp()
-
-					if prevX < 0 {
-						prevX = x
-					}
-
-					if prevX < lineLengths[y] {
-						x = prevX
+				if edit.gap.prevX < edit.lineLengths[edit.curY] {
+					edit.curX = edit.gap.prevX
+				} else {
+					if edit.lineLengths[edit.curY] == 1 {
+						edit.curX = 0
 					} else {
-						if lineLengths[y] == 1 {
-							x = 0
-						} else {
-							x = lineLengths[y]
-						}
+						edit.curX = edit.lineLengths[edit.curY]
 					}
-				} else if y == 0 && x > 0 {
-					x = 0
-					gap.MoveUp()
-					prevX = 0
 				}
-			case event.Key() == tcell.KeyDown:
-				if y != maxY && y < len(lineLengths)-1 {
-					y++
-					gap.MoveDown()
-
-					if prevX < 0 {
-						prevX = x
-					}
-
-					if prevX < lineLengths[y] {
-						x = prevX
-					} else if lineLengths[y] == 1 {
-						x = lineLengths[y] - 1
-					} else {
-						x = lineLengths[y]
-					}
-				} else if y == len(lineLengths)-1 && x < lineLengths[y] {
-					x = lineLengths[y]
-					prevX = x
-					gap.MoveDown()
-				}
-			case event.Key() == tcell.KeyLeft:
-				if x == 0 && y > 0 {
-					prevX = -1
-					y--
-					x = lineLengths[y]
-					gap.MoveLeft()
-				} else if x != 0 {
-					prevX = -1
-					x--
-					gap.MoveLeft()
-				}
-			case event.Key() == tcell.KeyRight:
-				if (lineLengths[y] == 1 || x == lineLengths[y]) && y < len(lineLengths)-1 {
-					prevX = -1
-					x = 0
-					y++
-					gap.MoveRight()
-				} else if x < lineLengths[y] {
-					prevX = -1
-					x++
-					gap.MoveRight()
-				}
-			case event.Key() == tcell.KeyEnter:
-				y++
-				x = 0
-				prevX = -1
-				insert('\n')
-			case event.Key() == tcell.KeyBackspace2:
-				if x > 0 {
-					gap.Delete(true)
-					x--
-					updateLines = true
-				} else if y > 0 {
-					y--
-					x = lineLengths[y]
-					gap.Delete(true)
-					updateLines = true
-				}
-				break
-			case event.Key() == tcell.KeyDelete:
-				if x != lineLengths[y] && y != len(lineLengths)-1 {
-					gap.Delete(false)
-					updateLines = true
-				}
-			default:
-				rn := event.Rune()
-				if rn == 0 {
-					continue
-				}
-				prevX = -1
-				insert(rn)
-				lineLengths[y]++
-				x++
-				prevX = x
+			} else if edit.curY == 0 && edit.curX > 0 {
+				edit.curX = 0
+				edit.gap.MoveUp()
+				edit.gap.prevX = 0
 			}
+		case event.Key() == tcell.KeyDown:
+			if edit.curY != edit.maxY && edit.curY < len(edit.lineLengths)-1 {
+				edit.curY++
+				edit.gap.MoveDown()
+
+				if edit.gap.prevX < 0 {
+					edit.gap.prevX = edit.curX
+				}
+
+				if edit.gap.prevX < edit.lineLengths[edit.curY] {
+					edit.curX = edit.gap.prevX
+				} else if edit.lineLengths[edit.curY] == 1 {
+					edit.curX = edit.lineLengths[edit.curY] - 1
+				} else {
+					edit.curX = edit.lineLengths[edit.curY]
+				}
+			} else if edit.curY == len(edit.lineLengths)-1 && edit.curX < edit.lineLengths[edit.curY] {
+				edit.curX = edit.lineLengths[edit.curY]
+				edit.gap.prevX = edit.curX
+				edit.gap.MoveDown()
+			}
+		case event.Key() == tcell.KeyLeft:
+			if edit.curX == 0 && edit.curY > 0 {
+				edit.gap.prevX = -1
+				edit.curY--
+				edit.curX = edit.lineLengths[edit.curY]
+				edit.gap.MoveLeft()
+			} else if edit.curX != 0 {
+				edit.gap.prevX = -1
+				edit.curX--
+				edit.gap.MoveLeft()
+			}
+		case event.Key() == tcell.KeyRight:
+			if (edit.lineLengths[edit.curY] == 1 || edit.curX == edit.lineLengths[edit.curY]) && edit.curY < len(edit.lineLengths)-1 {
+				edit.gap.prevX = -1
+				edit.curX = 0
+				edit.curY++
+				edit.gap.MoveRight()
+			} else if edit.curX < edit.lineLengths[edit.curY] {
+				edit.gap.prevX = -1
+				edit.curX++
+				edit.gap.MoveRight()
+			}
+		case event.Key() == tcell.KeyEnter:
+			edit.curY++
+			edit.curX = 0
+			edit.gap.prevX = -1
+			edit.insert('\n')
+		case event.Key() == tcell.KeyBackspace2:
+			if edit.curX > 0 {
+				edit.gap.Delete(true)
+				edit.curX--
+				edit.updateLines = true
+			} else if edit.curY > 0 {
+				edit.curY--
+				edit.curX = edit.lineLengths[edit.curY]
+				edit.gap.Delete(true)
+				edit.updateLines = true
+			}
+			break
+		case event.Key() == tcell.KeyDelete:
+			if edit.curX != edit.lineLengths[edit.curY] && edit.curY != len(edit.lineLengths)-1 {
+				edit.gap.Delete(false)
+				edit.updateLines = true
+			}
+		default:
+			rn := event.Rune()
+			if rn == 0 {
+				break
+			}
+			edit.gap.prevX = -1
+			edit.insert(rn)
+			edit.lineLengths[edit.curY]++
+			edit.curX++
+			edit.gap.prevX = edit.curX
 		}
-		update()
+	}
+
+	if edit.updateLines {
+		edit.lines = edit.gap.GetLines(edit.firstLn, edit.gap.GetLineCount())
+		tmp := make([]int, edit.lastLn)
+		copy(tmp, edit.lineLengths)
+		edit.lineLengths = tmp
+		edit.lineLengths = make([]int, len(edit.lines))
+		for i, j := edit.firstLn, 0; i <= edit.lastLn && j < len(edit.lines); i, j = i+1, j+1 {
+			edit.lineLengths[i] = len(edit.lines[j])
+		}
+
+		edit.updateLines = false
 	}
 }
 
-func insert(ch rune) {
-	gap.Insert(ch)
-	updateLines = true
+func (edit *Editor) GetCursorPos() (x, y int) {
+	return edit.curX, edit.curY
 }
 
-func update() {
-	if updateLines {
-		lines = gap.GetLines(lineStart, gap.GetLineCount())
-		tmp := make([]int, lineEnd)
-		copy(tmp, lineLengths)
-		lineLengths = tmp
-		lineLengths = make([]int, len(lines))
-		for i, j := lineStart, 0; i <= lineEnd && j < len(lines); i, j = i+1, j+1 {
-			lineLengths[i] = len(lines[j])
+func (edit *Editor) SetFocus() {
+	edit.focused = true
+}
+
+func (edit *Editor) RemoveFocus() {
+	edit.focused = false
+}
+
+func (edit *Editor) delete(backwards bool) {
+	edit.gap.Delete(backwards)
+	edit.updateLines = true
+}
+
+func (edit *Editor) insert(ch rune) {
+	edit.gap.Insert(ch)
+	edit.updateLines = true
+}
+
+func (edit *Editor) Render(screen tcell.Screen) {
+	screen.Fill(' ', *edit.style)
+	screen.Sync()
+
+	for i, v := range edit.lines {
+		if len(v) > 1 {
+			screen.SetContent(edit.xPos, edit.yPos+i, v[0], v[1:], *edit.style)
+		} else {
+			screen.SetContent(edit.xPos, edit.yPos+i, v[0], nil, *edit.style)
 		}
-
-		updateLines = false
-
-		screen.Fill(' ', defStyle)
-		updateBufferDisplay()
 	}
-
-	screen.ShowCursor(x, y)
 }
 
+/*
+func updateBufferDisplay() {
+screen.Fill(' ', defStyle)
+screen.Sync()
+for i, v := range lines {
+for j, ch := range v {
+if ch == '\n' {
+screen.SetContent(j, i, '$', nil, defStyle)
+} else {
+screen.SetContent(j, i, ch, nil, defStyle)
+}
+}
+//		if len(v) > 1 {
+//			screen.SetContent(0, i, v[0], v[1:], defStyle)
+//		} else {
+//			screen.SetContent(0, i, v[0], nil, defStyle)
+//		}
+}
+}
 func debugInfo() {
 	chars := gap.Chars()
 	gapStart, gapEnd := gap.GapStartAndEnd()
@@ -242,23 +248,4 @@ func debugInfo() {
 	screen.SetContent(debugX+38, debugY+5, prvXRunes[0], prvXRunes[1:], defStyle)
 }
 
-func updateBufferDisplay() {
-	screen.Fill(' ', defStyle)
-	screen.Sync()
-	for i, v := range lines {
-		for j, ch := range v {
-			if ch == '\n' {
-				screen.SetContent(j, i, '$', nil, defStyle)
-			} else {
-				screen.SetContent(j, i, ch, nil, defStyle)
-			}
-		}
-		/*
-			if len(v) > 1 {
-				screen.SetContent(0, i, v[0], v[1:], defStyle)
-			} else {
-				screen.SetContent(0, i, v[0], nil, defStyle)
-			}
-		*/
-	}
-}
+*/
